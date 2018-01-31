6cf8054f85c6a666f5925d1f0e652250824a257d
Prince <a_prince2@tss-hr328-dm8.local>
2018-01-31 08:29:16 -0500

    added head header and footer into includes

---
+++ includes/footer.php
@@ -0,0 +1,34 @@
+<footer id="mainFooter" class="row">
+  <h2 class="hide">Main Footer</h2>
+  <section class="small-12 show-for-small-only columns">
+    <h3 class="hide">Search Bar for Mobile</h3>
+    <form id="searchForm" action="search.php" method="get">
+      <div class="input-group">
+        <input class="input-group-field" type="text" placeholder="Search...">
+        <div class="input-group-button">
+          <input type="submit" class="button" id="searchBut" value="search">
+        </div>
+      </div>
+    </form>
+  </section>
+
+  <nav class="small-12 large-3 columns">
+    <h3 class="hide">Social Media Navigation</h3>
+    <ul id="socialNav">
+      <li id="facebook"><a href="http://www.facebook.com/teamcanada1972"><img src="images/facebook.png" alt="facebook"></a></li>
+      <li id="twitter"><a href="http://www.twitter.com/teamcanada1972"><img src="images/twitter.png" alt="twitter"></a></li>
+      <li id="instagram"><a href="http://www.instagram.com"><img src="images/instagram.png" alt="instagram"></a></li>
+    </ul>
+  </nav>
+
+  <div class="large-9 large-push-1 columns hide-for-small-only">
+    <p>sign up</p>
+  </div>
+</footer>
+
+<section class="row" id="copyrightBar">
+  <h2 class="hide">Copyright Bar</h2>
+  <div class="small-12 columns">
+    <p>&copy; 1972 Summit Series Hockey Team Inc. All Rights Reserved.</p>
+  </div>
+</section>
--- theteam.php
+++ theteam.php
@@ -1,79 +1,14 @@
 <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MEET THE TEAM - Team Canada 1972</title>
-    <link href="http://fonts.googleapis.com/css?family=Fjalla+One%7CPlayfair+Display" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="css/foundation-icons.css">
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/css/app.css">
+    <?php include 'includes/head.php';?>
+    <title>HOME - Team Canada 1972</title>
   </head>
 
   <body>
     <h1 class="hide">Team Canada 1972</h1>
 
-    <!--Mobile Header and Nav-->
-    <header class="row mainHeader show-for-small-only" data-sticky-container>
-      <h2 class="hide">Mobile Header</h2>
-      <nav class="small-12 columns">
-        <h2 class="hide">Mobile Navigation</h2>
-        <div class="title-bar" data-responsive-toggle="mobileNav">
-          <button class="menu-icon float-left" type="button" data-toggle></button>
-          <img src="images/logo.svg" alt="logo" width="60" height="61" class="title-bar-title" id="logoMobile">
-        </div>
-
-        <div id="mobileNav">
-          <ul class="mainNav">
-            <li><a href="index.html">HOME</a></li>
-            <li><a href="theteam.html">THE TEAM</a></li>
-            <li><a href="summitseries.html">SUMMIT SERIES</a></li>
-            <li><a href="legacy.html">LEGACY</a></li>
-            <li><a href="gallery.html">GALLERY</a></li>
-            <li><a href="media.html">MEDIA CENTRE</a></li>
-            <li><a href="partners.html">PARTNERS</a></li>
-            <li><a href="#">MERCHANDISE</a></li>
-            <li><a href="contact.html">CONTACT</a></li>
-            <li><a href="#" id="mobilePortalBut">PORTAL</a></li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-<!--Mobile Header and Nav CLOSE-->
-
-<!--Medium/Large Header and Nav-->
-    <div class="off-canvas-wrapper">
-      <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-        <nav class="off-canvas position-left mainNav" id="offCanvasNav" data-off-canvas>
-          <h2 class="hide">Medium/Large Navigation</h2>
-          <ul class="vertical menu" data-menu>
-            <li><a href="index.html">HOME</a></li>
-            <li><a href="theteam.html">THE TEAM</a></li>
-            <li><a href="summitseries.html">SUMMIT SERIES</a></li>
-            <li><a href="legacy.html">LEGACY</a></li>
-            <li><a href="gallery.html">GALLERY</a></li>
-            <li><a href="media.html">MEDIA CENTRE</a></li>
-            <li><a href="partners.html">PARTNERS</a></li>
-            <li><a href="#">MERCHANDISE</a></li>
-            <li><a href="contact.html">CONTACT</a></li>
-          </ul>
-        </nav>
-
-        <div class="off-canvas-content" data-off-canvas-content>
-          <header class="mainHeader hide-for-small-only expanded" data-sticky-container>
-            <h2 class="hide">Header - Medium/Large Screens</h2>
-            <div class="large-12 columns" data-sticky data-options="stickTo:top;stickyOn:small;marginTop:0" style="width:100%">
-              <div class="title-bar">
-                <a href="#" id="portalBut">PORTAL</a>
-                <button class="menu-icon" type="button" data-toggle="offCanvasNav"></button>
-                <div id="logos">
-                  <img data-interchange="[images/logo_medium.svg, medium], [images/logo_large.svg, large]" alt="Alternate Logos" id="logoToggle">
-                </div>
-              </div>
-            </div>
-          </header>
-<!--Medium/Large Header and Nav CLOSE-->
+    <?php include 'includes/header.php';?>
 
           <div><!--content goes here-->
 
@@ -596,7 +531,7 @@
                       <img src="images/5-park-back.png" alt="Park Stats"/>
                     </div>
                   </div>
-                   
+
                   <div class="card large-3 medium-4 columns two">
                     <div class="front">
                       <img src="images/6-ellis.png" alt="Ellis"/>
@@ -892,36 +827,7 @@
             </section><!-- end of player con-->
           </div>
 
-          <footer id="mainFooter" class="row">
-            <h2 class="hide">Main Footer</h2>
-            <section class="small-12 show-for-small-only columns">
-              <h3 class="hide">Search Bar for Mobile</h3>
-              <form id="searchForm" action="search.php" method="get">
-                <div class="input-group">
-                  <input class="input-group-field" type="text" placeholder="Search...">
-                  <div class="input-group-button">
-                    <input type="submit" class="button" id="searchBut" value="search">
-                  </div> 
-                </div>
-              </form>
-            </section>
-
-            <nav class="small-12 large-3 columns end">
-              <h3 class="hide">Social Media Navigation</h3>
-              <ul id="socialNav">
-                <li id="facebook"><a href="http://www.facebook.com/teamcanada1972"><img src="images/facebook.png" alt="facebook"></a></li>
-                <li id="twitter"><a href="http://www.twitter.com/teamcanada1972"><img src="images/twitter.png" alt="twitter"></a></li>
-                <li id="instagram"><a href="http://www.instagram.com"><img src="images/instagram.png" alt="instagram"></a></li>
-              </ul>
-            </nav>
-          </footer>
-
-          <section class="row" id="copyrightBar">
-            <h2 class="hide">Copyright Bar</h2>
-            <div class="small-12 columns">
-              <p>© 1972 Summit Series Hockey Team Inc. All Rights Reserved.</p>
-            </div>
-          </section>
+              <?php include 'includes/footer.php';?>
 
         </div>
       </div>
