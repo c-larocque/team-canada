e6b841e500a0bc7d771698aedda3e1c2d6ee8e04
jpeden96 <j.peden96@gmail.com>
2018-01-31 11:06:01 -0500

    created the portal page, added simple content

---
+++ portal.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <?php include 'includes/head.php';?>
+    <title>STUDENT PORTAL</title>
+  </head>
+
+  <body>
+    <h1 class="hide">STUDENT PORTAL</h1>
+
+      <?php include 'includes/header.php';?>
+
+      <section class="porthead row">
+        <div class="small-12 columns portalinfo">
+        <h1>STUDENT PORTAL - COMING SOON</h1>
+        <div class="text-center">
+        <img src="images/portal-homepage-logo.png" alt="Portal Logo" class="portallogo">
+        <p>The Student Portal is an interactive page for students to learn more about Team Canada 1972. This page focuses on the importance teamwork, with a highlight on the 28,800 seconds that Team Canada 1972 played on the ice during the Summit Series. The portal is used to encourage students to find their own passions, where they can take the skills and lessons they learn in the portal and through the story of Team Canada 1972, and bring then into their own experiences. </p>
+        <p>The portal has a parent and student side. The parent side explains all of the features of the portal, breaking down what the students learn from each element. The student side has a kid-friendly, simple design and allows the students to either play the interactive game, scroll through fun facts or Team Canada 1972 history, or upload images to a student gallery.</p>
+      </div>
+      </div>
+      </section>
+
+      <?php include 'includes/footer.php';?>
+
+  <script src="js/vendor/jquery.js"></script>
+  <script src="js/vendor/what-input.js"></script>
+  <script src="js/vendor/foundation.js"></script>
+  <script src="js/app.js"></script>
+</body>
+</html>
